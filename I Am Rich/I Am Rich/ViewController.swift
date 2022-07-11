d39f8a1a372d2882c16533260004c3ea1c29d082
dandisinaga <dandisinaga@icloud.com>
2022-07-12 00:27:37 +0700

    Project 1

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Mochamad Dandi on 02/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
